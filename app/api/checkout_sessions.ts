615ad1e9d36cc74b286caa27fe376c52a8587956
justinhSE <justinh.tech1@gmail.com>
2024-08-13 18:53:44 -0400

    mike changes

--- app/api/checkout_sessions.ts
+++ app/api/checkout_sessions.ts
@@ -1,48 +1,18 @@
-import { NextResponse } from 'next/server'
+import { NextResponse, type NextRequest } from 'next/server'
 import Stripe from 'stripe'
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: '2022-11-15',
+const API_KEY = process.env.STRIPE_SECRET_KEY as string || ''
+
+const stripe = new Stripe(API_KEY, {
+  apiVersion: '2024-06-20',
 })
 
-export async function POST(req) {
+export async function POST(req: NextRequest) {
   try {
     // We'll implement the checkout session creation here
-    const params = {
-        mode: 'subscription',
-        payment_method_types: ['card'],
-        line_items: [
-          {
-            price_data: {
-              currency: 'usd',
-              product_data: {
-                name: 'Pro subscription',
-              },
-              unit_amount: 1000, // $10.00 in cents
-              recurring: {
-                interval: 'month',
-                interval_count: 1,
-              },
-            },
-            quantity: 1,
-          },
-        ],
-        success_url: `${req.headers.get(
-          'Referer',
-        )}result?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${req.headers.get(
-          'Referer',
-        )}result?session_id={CHECKOUT_SESSION_ID}`,
-      }
-      
-      const checkoutSession = await stripe.checkout.sessions.create(params)
-      
-      return NextResponse.json(checkoutSession, {
-        status: 200,
-      })
   } catch (error) {
     console.error('Error creating checkout session:', error)
-    return new NextResponse(JSON.stringify({ error: { message: error.message } }), {
+    return new NextResponse(JSON.stringify({ error: { message: `${error instanceof Error ? error.message : 'Unknown error'}` } }), {
       status: 500,
     })
   }
