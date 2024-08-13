cefeae3d37c8cdcabb2b1e092fc8005844b723c7
justinhSE <justinh.tech1@gmail.com>
2024-08-13 18:49:53 -0400

    folder move

---
+++ app/api/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server'
+import OpenAI from 'openai'
+
+const systemPrompt = `
+You are a flashcard creator, you take in text and create multiple flashcards from it. Make sure to create exactly 10 flashcards.
+Both front and back should be one sentence long.
+You should return in the following JSON format:
+{
+  "flashcards":[
+    {
+      "front": "Front of the card",
+      "back": "Back of the card"
+    }
+  ]
+}
+`
